e39eae31844730172a4631a42a87172c281f96c5
loviedo-VS <lucia@visionsoftsolutions.com>
2025-11-03 14:13:01 -0600

    Initial Spring Boot setup

---
+++ src/main/java/com/springboot_interview_task_mvn/SpringbootInterviewTaskMvnApplication.java
@@ -0,0 +1,13 @@
+package com.springboot_interview_task_mvn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootInterviewTaskMvnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootInterviewTaskMvnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot_interview_task_mvn/SpringbootInterviewTaskMvnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot_interview_task_mvn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootInterviewTaskMvnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
